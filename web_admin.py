5680392257361f2027e2f7ace67c0343c913adc7
vsagar <M14xR2>
2013-09-25 13:17:31 -0400

    second commit

---
+++ web_admin.py
@@ -0,0 +1,19 @@
+from flask import request
+from flask import Flask
+from flask import render_template
+import logging
+import json
+
+
+app = Flask(__name__)
+logging.basicConfig(filename='example.log',level=logging.DEBUG)
+
+
+@app.route("/", methods = ["POST","GET"])
+def index():
+	return render_template("index.html")
+
+
+if __name__ == "__main__":
+	app.debug = True
+	app.run(host='0.0.0.0', port=5000)
